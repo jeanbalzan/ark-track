8cd92414cff127f4e5dd16577a9552c5cda1b3db
jeanbalzan <jeanbbalzan@gmail.com>
2022-07-09 15:45:39 +0100

    updated security

--- config.py
+++
@@ -1,9 +0,0 @@
-# API_KEY = 'PKPVMAAVGLUUFX1J74FI'
-API_KEY = 'nVJMf3mpP8YWGh66v4lkmpAcZNMa4_Oh'
-SECRET_KEY = '4PfwNnDtATHVnQevdpbwjYx8j3CkOMLgOv3RfIfU'
-API_URL = 'https://paper-api.alpaca.markets'
-
-DB_HOST = 'localhost'
-DB_USER = 'postgres'
-DB_PASS = 'password'
-DB_NAME = 'etfdb'
